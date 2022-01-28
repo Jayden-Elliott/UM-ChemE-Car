0dce2e1d4b92dac005cd74ec7689da768fdbac5a
Jayden Elliott <jdelliott2003@gmail.com>
2022-01-27 19:06:16 -0500

    commit

--- car/car.ino
+++ car/car.ino
@@ -1,56 +1,50 @@
-void setup(){
-Serial.begin(9600);
-Serial.println("CLEARDATA");
-Serial.println("LABEL,Date,Time,Data");
-Serial.println("RESETTIMER");
-pinMode(7, OUTPUT);
-
+void setup() {
+    Serial.begin(9600);
+    Serial.println("CLEARDATA");
+    Serial.println("LABEL,Date,Time,Data");
+    Serial.println("RESETTIMER");
+    pinMode(7, OUTPUT);
 }
- int timer = 0;
+int timer = 0;
 
- int a = 0;
- int max = 0;
- int counter = 0;
- int stopcar = 0;
- int starttime = 0;
- int mincount = 0; 
- int maxcount = 0; 
- int minfind = 1200;
- int maxfind = 0;
-void loop(){
-int sensorValue = analogRead(A0);
-int minprev = sensorValue;
-if (minfind > minprev){
-  minfind = minprev; 
-  mincount = 0;
-  digitalWrite(7, HIGH);
-  
-}
-else{
-  mincount++;
-}
+int a = 0;
+int max = 0;
+int counter = 0;
+int stopcar = 0;
+int starttime = 0;
+int mincount = 0;
+int maxcount = 0;
+int minfind = 1200;
+int maxfind = 0;
+void loop() {
+    int sensorValue = analogRead(A0);
+    int minprev = sensorValue;
+    if (minfind > minprev) {
+        minfind = minprev;
+        mincount = 0;
+        digitalWrite(7, HIGH);
 
-if (mincount > 120){
-  digitalWrite(7, LOW);
-  if (1010 <= sensorValue){
-    maxfind = sensorValue; 
-    timer = 9999;
-   
-  }
-Serial.print(","); Serial.println("SCROLLDATA_20");
+    } else {
+        mincount++;
+    }
 
-}
+    if (mincount > 120) {
+        digitalWrite(7, LOW);
+        if (1010 <= sensorValue) {
+            maxfind = sensorValue;
+            timer = 9999;
+        }
+        Serial.print(",");
+        Serial.println("SCROLLDATA_20");
+    }
 
+    Serial.print("DATA,DATE,TIME,");
+    Serial.print(sensorValue);
 
-Serial.print("DATA,DATE,TIME,");
-Serial.print(sensorValue);
+    Serial.print(",");
+    Serial.println(timer++);
 
-Serial.print(",");
-Serial.println(timer++); 
-        
-//Serial.println(sensorValue);
-//Serial.println(timer);
-delay(1000);
-
-
+    // Serial.println(sensorValue);
+    // Serial.println(timer);
+    delay(1000);
 }
