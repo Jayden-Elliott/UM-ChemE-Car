0909b75934fc12bfd07f43cfca9cd3c8d44a2c57
Jayden Elliott <jdelliott2003@gmail.com>
2022-02-10 20:58:33 -0500

    reorganized declarations

--- car/car.ino
+++ car/car.ino
@@ -1,28 +1,30 @@
-#include <Arduino_AVRSTL.h>
 #include <SoftwareSerial.h>
 using namespace std;
 
-void setup() {
-    Serial.begin(9600);
-    pinMode(7, OUTPUT);
-    digitalWrite(7, HIGH);
-    Serial.println("TIME,READING");
-    last_10.resize(10);
-}
-
-int interval = 100;  // milliseconds
-int timer = 0;
+double interval;  // milliseconds
+double timer;
 
 int falling_count = 0;
 int last_avg = INT32_MAX;
 int time_start_val = 0;
 
-vector<int> last_10;
+int last_10[10];
+double avg_index;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(7, OUTPUT);
+    digitalWrite(7, HIGH);
+    Serial.println("START, LOG WITH CONTROL-A SHIFT-H");
+    delay(5000);
+    Serial.println("TIME,READING");
+    timer = 0;
+    interval = 20;
+}
 
 void loop() {
     int sensor_val = analogRead(A0);
-    last_10.erase(0);
-    last_10.push_back(sensor_val);
+    /* last_10[avg_index++] = sensor_val;
     int avg = rolling_average();
 
     if (avg <= last_avg) {
@@ -30,20 +32,19 @@ void loop() {
         ++falling_count;
     } else {
         falling_count = 0;
-    }
+    } */
 
-    if (falling_count > 100 && sensor_val <= 1100) {
+    /* if (falling_count > 100 && sensor_val <= 1100) {
         digitalWrite(7, LOW);
         time_start_val = sensor_val;
         Serial.println();
         Serial.println("TIMER STARTED");
-        timing();
-    }
+    } */
 
-    Serial.println(timer + "," + sensor_val);
+    Serial.println(String(timer) + "," + String(sensor_val));
 
     delay(interval);
-    timer += interval / 1000;
+    timer += double(interval) / 1000;
 }
 
 int rolling_average() {
@@ -52,9 +53,4 @@ int rolling_average() {
         sum += x;
     }
     return sum / 10;
-}
-
-void timing() {
-    timer = 0;
-    while (true) {}
 }
