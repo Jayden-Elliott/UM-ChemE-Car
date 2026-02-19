b47dc49f51ca3e471600221eb54ebacd82f7eaa3
Arya <arukad@umich.edu>
2026-02-18 21:46:00 -0500

    i have no fucking clue

--- VC-Clock/car/src/NEW.cpp
+++ VC-Clock/car/src/NEW.cpp
@@ -12,15 +12,15 @@
 //   while (!Serial) {
 //     delay(1);
 //   }
-  
 //   if (!as7341.begin()){
-//     Serial.println("Could not find AS7341");
-//     while (1) { delay(10); }
-//   }
-
-//   as7341.setATIME(100);
-//   as7341.setASTEP(999);
-//   as7341.setGain(AS7341_GAIN_256X);
+//       Serial.println("Could not find AS7341");
+//       while (1) { delay(10); }
+//     }
+    
+//     as7341.setATIME(100);
+//     as7341.setASTEP(999);
+//     as7341.setGain(AS7341_GAIN_256X);
+//     Serial.println("HELLO");
 // }
 
 // void loop() {
